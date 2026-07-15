2a4c42176f6f11bf840ba8c91809b17a1e56d1a0
TheinWin65 <win84209@gmail.com>
2026-07-16 02:55:17 +0800

    Update app.py and add history.html

--- app.py
+++ app.py
@@ -8,7 +8,6 @@ def index():
 
 @app.route('/history')
 def history():
-    # ဤနေရာတွင် Database မှ Data များကို နောက်မှ ဆက်လက်ချိတ်ဆက်ပါမည်
     return render_template('history.html')
 
 if __name__ == '__main__':
